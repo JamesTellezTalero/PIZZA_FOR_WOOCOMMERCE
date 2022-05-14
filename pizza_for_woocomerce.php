57e8f7616df982a233244ed4e57e3867dd2aa719
Ex <jtalero10@outlook.es>
2022-05-13 19:48:08 -0500

    declaracion basica

---
+++ pizza_for_woocomerce.php
@@ -0,0 +1,17 @@
+<?php 
+
+/**
+ * Plugin Name:       Pizza For Woocommerce
+ * Plugin URI:        https://github.com/JayEx-dev/PIZZA_FOR_WOOCOMMERCE
+ * Description:       Pizza mannagment for Woocommerce
+ * Version:           1.0.0
+ * Requires at least: 5.3
+ * Requires PHP:      8.1
+ * Author:            James tellez
+ * Author URI:        https://github.com/JayEx-dev
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/JayEx-dev/PIZZA_FOR_WOOCOMMERCE
+ * Text Domain:       pizza-for-woocommerce
+ * Domain Path:       /languages
+ */
